7c0d84c09a75adcbe9c930895ee5fc3853c2c4b4
Tapan Prajapati <tapanprajapati22@gmail.com>
2023-12-31 17:13:23 +0530

    ME_QTIFY_MODULE_HEROSECTION

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './Hero.module.css';
+import HeroImage from '../../assets/hero_headphones.png';
 
 function Hero(){
     return(
         <div className={styles.hero}>
-            <div>
+            <div className={styles.HeroTitle}>
                 <h1>100 Thousand Songs, ad-free</h1>
                 <h1>Over thousands podcast episodes</h1>
             </div>
             <div>
-                <img src={require('../../assets/hero_headphones.png')} width={112} alt='headphones'/>
+                <img className={styles.HeroImage} src={HeroImage} alt='headphones'/>
             </div>
         </div>
     )
