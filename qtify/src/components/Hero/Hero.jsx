b63d31142e435945b648e8021110c6619596e5bf
Tapan Prajapati <tapanprajapati22@gmail.com>
2024-01-01 12:36:14 +0530

    make some changes

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import styles from './Hero.module.css';
-import HeroImage from '../../assets/hero_headphones.png';
+import React from "react";
+import styles from "./Hero.module.css";
+import HeroImage from "../../assets/hero_headphones.png";
 
-function Hero(){
-    return(
-        <div className={styles.hero}>
-            <div className={styles.HeroTitle}>
-                <h1>100 Thousand Songs, ad-free</h1>
-                <h1>Over thousands podcast episodes</h1>
-            </div>
-            <div>
-                <img className={styles.HeroImage} src={HeroImage} alt='headphones'/>
-            </div>
-        </div>
-    )
+function Hero() {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.HeroTitle}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousand podcast episodes</h1>
+      </div>
+      <div>
+        <img className={styles.HeroImage} src={HeroImage} alt="headphones" />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
